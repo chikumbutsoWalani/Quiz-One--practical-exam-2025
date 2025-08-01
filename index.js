7f654a7db717b0e3c810c19ccc90446cf7da4955
Chikumbutso Walani <walanchiku@gmail.com>
2025-08-01 10:48:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+// TODO: Add event listener for theme change button
 
+// TODO: Add jQuery code for highlighting list items
+
+// TODO: Add code for reset button
+
+// TODO: Add code for adding new list items from form
